cba0f23b8a25a11efe9bdd9032f0b51f45446b84
Finoarabb <135574261+Finoarabb@users.noreply.github.com>
2025-05-18 11:50:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.get('/api/whoami',function(req,res){
-  res.json({ipadress:req.ip,language:req.acceptsLanguages(),software:req.headers['user-agent']});
+  res.json({ipadress:req.ips,language:req.acceptsLanguages(),software:req.headers['user-agent']});
 })
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
